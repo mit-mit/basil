0f8e4b381f026923f88fe1e24c97a66ad768f4df
Michael Thomsen <mit@google.com>
2021-06-23 09:31:25 +0200

    Template files

---
+++ swiftuiapp/Basil/Basil/BasilApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasilApp.swift
+//  Basil
+//
+//  Created by Michael Thomsen on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BasilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
